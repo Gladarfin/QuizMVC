191141ebc892ec67718285942725bc8225be0382
BartimaeusNoteBook <gladarfinvs@gmail.com>
2023-01-18 14:07:04 +0500

    First Commit

---
+++ QuizMVC.App/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace QuizMVC.App.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ QuizMVC.Data/QuizMVCDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace QuizMVC.Data;
+
+public class QuizMvcDbContext: DbContext
+{
+    public QuizMvcDbContext(DbContextOptions<QuizMvcDbContext> options)
+    :base(options)
+    {
+        
+    }
+    
+    //DbSets Here
+}
